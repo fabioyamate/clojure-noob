d677b54971b6fdc0f84e2e2d75894535b749c53d
Fabio Yamate <fabioyamate@gmail.com>
2016-05-31 18:31:06 -0300

    Free datomic

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [funcool/cats "1.2.1"]
                  [manifold "0.1.3"]
                  [org.mock-server/mockserver-client-java "3.10.4"]
-                 [com.datomic/datomic-pro "0.9.5350"]
+                 [com.datomic/datomic-free "0.9.5350"]
                  [reduce-fsm "0.1.4"]
 
                  [io.dropwizard.metrics/metrics-core "3.1.2"]
