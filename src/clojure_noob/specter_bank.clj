0b5c8c1ba2562daf024b523befbc4469a423a17a
Fabio Yamate <fabioyamate@gmail.com>
2016-07-03 22:06:59 -0300

    Bank specter example

---
+++ src/clojure_noob/specter_bank.clj
@@ -0,0 +1,124 @@
+(ns clojure-noob.specter-bank
+  (:require [com.rpl.specter :as s]
+            [com.rpl.specter.macros :as sm]))
+
+(def world
+  {:people [{:money 129827 :name "Alice Brown"}
+            {:money 100 :name "John Smith"}
+            {:money 6821212339 :name "Donald Trump"}
+            {:money 2870 :name "Charlie Johnson"}
+            {:money 8273821 :name "Charlie Rose"}
+            ]
+   :bank {:funds 4782328748273}})
+
+(defn user->bank [world name amt]
+  (let [;; First, find out how much money that user has
+        ;; to determine whether or not this is a valid transfer
+        curr-funds (->> world
+                        :people
+                        (filter (fn [user] (= (:name user) name)))
+                        first
+                        :money
+                        )]
+    (if (< curr-funds amt)
+      (throw (IllegalArgumentException. "Not enough funds!"))
+      ;; If valid, then need to subtract the transfer amount from the
+      ;; user and add the amount to the bank
+      (-> world
+          (update
+           :people
+           (fn [user-list]
+             ;; Important to use mapv to maintain the type of the
+             ;; sequence containing the list of users. This code
+             ;; modifies the user matching the name and keeps
+             ;; every other user in the sequence the same.
+             (mapv (fn [user]
+                     ;; Notice how nested this code is that manipulates the users
+                     (if (= (:name user) name)
+                       (update user :money #(+ % amt))
+                       ;; If a user doesn't match the name during the scan,
+                       ;; don't modify them
+                       user
+                       ))
+                   user-list)))
+          (update-in
+           [:bank :funds]
+           #(- % amt))
+          ))))
+
+(-> world
+    (user->bank "John Smith" 10)
+    (user->bank "Alice Brown" -5000))
+
+
+(defn transfer
+  "Note that this function works on *any* world structure. This handles
+   arbitrary many to many transfers of a fixed amount without overdrawing anyone"
+  [world from-path to-path amt]
+  (let [;; Get the sequence of funds for all entities making a transfer
+        givers (sm/select from-path world)
+
+        ;; Get the sequence of funds for all entities receiving a transfer
+        receivers (sm/select to-path world)
+
+        ;; Compute total amount each receiver will be credited
+        total-receive (* amt (count givers))
+
+        ;; Compute total amount each transferrer will be deducted
+        total-give (* amt (count receivers))]
+
+    ;; Make sure every transferrer has sufficient funds
+    (if (every? #(>= % total-give) givers)
+      (->> world
+           ;; Deduct from transferrers
+           (sm/transform from-path #(- % total-give))
+           ;; Credit the receivers
+           (sm/transform to-path #(+ % total-receive))
+           )
+      (throw (IllegalArgumentException. "Not enough funds!"))
+      )))
+
+(defn pay-fee [world]
+  (transfer world
+            [:people s/ALL :money]
+            [:bank :funds]
+            1))
+
+(defn bank-give-dollar [world]
+  (transfer world
+            [:bank :funds]
+            [:people s/ALL :money]
+            1))
+
+(-> world
+    pay-fee
+    bank-give-dollar)
+
+(defn user [name]
+  [:people
+   s/ALL
+   #(= (:name %) name)])
+
+(defn transfer-users [world from to amt]
+  (transfer world
+            [(user from) :money]
+            [(user to) :money]
+            amt))
+
+(transfer-users world "Alice Brown" "John Smith" 100000)
+
+(defn user->bank* [world from amt]
+  (transfer world
+            [(user from) :money]
+            [:bank :funds]
+            amt))
+
+(user->bank world "John Smith" 50)
+
+(defn bank-loyal-bonus [world]
+  (transfer world
+            [:bank :funds]
+            [:people (s/srange 0 3) s/ALL :money]
+            5000))
+
+(bank-loyal-bonus world)
--- src/clojure_noob/specter_sample.clj
+++ src/clojure_noob/specter_sample.clj
@@ -17,6 +17,26 @@
               [2 1 3 6 9 4 8])
 ;; => [2 1 3 6 10 4 8]
 
+(sm/transform [(s/srange 4 11) (s/filterer even?)]
+              reverse
+              [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15])
+;; srange -> [4 5 6 7 8 9 10]
+;; filterer(event) -> [4 6 8 10]
+;; rever -> [10 8 6 4]
+;; => [0 1 2 3 10 5 8 7 6 9 4 11 12 13 14 15]
+
+(sm/select [s/ALL s/ALL #(= 0 (mod % 3))]
+           [[1 2 3 4] [] [5 3 2 18] [2 4 6] [12]])
+;; s/ALL -> each element
+;; s/all -> each subelement
+;; only divisible by 3
+;; [3 3 18 6 12]
+
+(for [xs [[1 2 3 4] [] [5 3 2 18] [2 4 6] [12]]
+      x xs
+      :when (zero? (mod x 3))]
+  x)
+
 (sm/setval (s/srange 2 4) [:a :b :c :d :e] [0 1 2 3 4 5 6 7 8 9])
 ;; => [0 1 :a :b :c :d :e 4 5 6 7 8 9]
 
