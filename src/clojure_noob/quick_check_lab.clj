40d3d938137ad5c1a774f05aae7921b976ddb8de
Fabio Yamate <fabioyamate@gmail.com>
2017-06-24 18:53:29 -0300

    Add more generators test

--- src/clojure_noob/quick_check_lab.clj
+++ src/clojure_noob/quick_check_lab.clj
@@ -58,7 +58,16 @@
                      (gen/fmap set
                                (gen/vector (gen/map gen/string
                                                     (gen/such-that not-empty
-                                                                   (gen/vector gen/nat)))))) 4)
+                                                                   (gen/vector gen/nat))))))
+            4)
+
+(-> (->> (gen/vector gen/nat)
+         (gen/such-that not-empty)
+         (gen/map gen/string)
+         (gen/vector)
+         (gen/fmap set)
+         (gen/map gen/keyword))
+    (gen/sample 4))
 
 (gen/sample (gen/such-that
              (every-pred not-empty
@@ -99,3 +108,60 @@ gen/pos-int
 (gen/sample (gen/not-empty (gen/vector gen/s-pos-int)))
 
 (gen/sample  (gen/return 3)) ;; => (3 3 3 3 3 3 3 3 3)
+
+(def definition-data
+  {:min 100 :max 500})
+
+(gen/sample (gen/choose 100 500) 10)
+
+(gen/sample gen/char 10)
+(gen/sample gen/boolean 10)
+(gen/sample gen/char-ascii 10)
+(gen/sample gen/keyword)
+(gen/sample gen/large-integer)
+(gen/sample gen/keyword-ns)
+(gen/sample gen/neg-int)
+(gen/sample gen/pos-int)
+#_(gen/sample (gen/sample-seq gen/int 10))
+(gen/sample (gen/frequency [[5 gen/int] [3 (gen/vector gen/int)] [2 gen/boolean]]))
+(gen/sample (gen/hash-map :a gen/boolean :b gen/nat))
+
+(gen/sample
+ (gen/let [users (gen/list (gen/hash-map :name gen/string-ascii
+                                         :age gen/nat))]
+   (->> users
+        (map #(assoc %2 :id %1) (range))
+        (gen/shuffle))) 10)
+
+(def prop-sorted-first-less-than-last
+  (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+    (let [s (sort v)]
+      (< (first s) (last s)))))
+
+(tc/quick-check 100 prop-sorted-first-less-than-last)
+
+(def gen-key gen/string)
+(def gen-value gen/string)
+(def gen-clear (gen/return [:clear!]))
+(def gen-size (gen/return [:size]))
+(def gen-store (gen/tuple (gen/return :store!) gen-key gen-value))
+(def gen-delete (gen/tuple (gen/return :delete!) gen-key))
+(def gen-fetch (gen/tuple (gen/return :fetch) gen-key))
+(def gen-ops (gen/vector (gen/one-of [gen-clear gen-store gen-delete gen-fetch gen-size])))
+
+(gen/sample gen-ops)
+
+(defn hm-run [db ops]
+  (reduce
+   (fn [hm [op k v]]
+     (case op
+       :clear! {}
+       :size hm
+       :store! (assoc hm k v)
+       :delete! (dissoc hm k)
+       :fetch hm))
+   db ops))
+
+(defspec hash-map-equiv 100
+  (prop/for-all [ops gen-ops]
+    (let [hm (hm-run {} ops)])))
