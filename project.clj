e3ee92c0e84e65ba97de7ddcceaca560212f0157
Fabio Yamate <fabioyamate@gmail.com>
2017-11-15 00:45:56 -0200

    Bump deps

--- project.clj
+++ project.clj
@@ -5,11 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :creds :auth}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/core.logic "0.8.11"]
                  [org.clojure/core.typed "0.4.1"]
                  [org.clojure/test.check "0.9.0"]
+                 [org.clojure/core.cache "0.6.5"]
+                 [org.clojure/core.memoize "0.5.8"]
                  [prismatic/schema "1.1.6"]
                  [org.clojure/algo.monads "0.1.6"]
                  [org.clojure/data.zip "0.1.2"]
@@ -17,14 +20,14 @@
                  [funcool/cats "1.2.1"]
                  [manifold "0.1.6"]
                  [org.mock-server/mockserver-client-java "3.11"]
-                 ;; [com.datomic/datomic-free "0.9.5350"]
-                 [com.datomic/datomic-pro "0.9.5407"]
+                 [com.datomic/datomic-free "0.9.5407"]
+                 ;; [com.datomic/datomic-pro "0.9.5407"]
                  [reduce-fsm "0.1.4"]
                  [clj-http "3.7.0"]
 
                  [io.dropwizard.metrics/metrics-core "3.2.5"]
                  [io.dropwizard.metrics/metrics-jvm "3.2.5"]
-                 [com.aphyr/riemann-java-client "0.4.5"]
+                 #_[com.aphyr/riemann-java-client "0.4.5"]
                  [com.aphyr/metrics3-riemann-reporter "0.4.1"]
                  [riemann-clojure-client "0.4.2"]
 
