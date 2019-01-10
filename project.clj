cd2e9134f442af71d738f7bc9d312e50fd0fc2fc
Fabio Yamate <fabioyamate@gmail.com>
2019-01-09 23:58:46 -0200

    Remove finalge

--- project.clj
+++ project.clj
@@ -32,10 +32,6 @@
 
                  [http-kit "2.2.0"]
 
-                 [finagle-clojure/core "0.7.0"]
-                 [finagle-clojure/http "0.7.0"]
-                 [io.zipkin.finagle/zipkin-finagle-http_2.11 "0.3.6"]
-
                  [criterium "0.4.4"]]
 
   :eftest {:test-warn-time 500
