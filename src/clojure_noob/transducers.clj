71bb80054ee7f29b1c4ad6f43feffb42ce341eac
Fabio Yamate <fabioyamate@gmail.com>
2020-08-03 13:49:43 -0300

    Transducers samples

--- src/clojure_noob/transducers.clj
+++ src/clojure_noob/transducers.clj
@@ -1,10 +1,72 @@
-(ns clojure-noob.transducers)
+(ns clojure-noob.transducers
+  (:require [clojure.core.async :as async :refer [>! <! >!! <!! go go-loop]]))
+
+;; https://clojure.org/reference/transducers
+;; https://github.com/matthiasn/talk-transcripts/blob/master/Hickey_Rich/Transducers.md
 
 (defn inc-all [xs]
   (map inc xs))
 
+;; xform = transformation
 (def xform1
   (comp (partition-all 5)
         (map inc-all)))
 
-(transduce xform1 conj nil (range 27))
+(comment
+  ;; "it is a reduce" with transformation
+  (transduce xform1 conj nil (range 27))
+
+  )
+
+(defn my-inc [x]
+  (println "my-inc: " x)
+  (inc x))
+
+(defn my-even? [x]
+  (println "my-even: " x)
+  (even? x))
+
+(defn my-sum
+  [& x]
+  (println "my-sum: " x)
+  (apply + x))
+
+(def xform-sum
+  (comp (map my-inc)
+        (filter my-even?)
+        (map (partial my-sum 5))))
+
+(transduce xform-sum
+           vector
+           (range 5))
+
+;; traditional way
+
+(->> (range 5)
+     (map my-inc)
+     (filter my-even?)
+     (map (partial my-sum 5))
+     (reduce vector []))
+
+;; eduction
+
+(def iter (eduction xform-sum (range 5)))
+(reduce vector [] iter)
+
+;; into
+
+(into [] xform-sum (range 5))
+
+;; sequence
+
+(sequence xform-sum (range 5))
+
+;; async go channel
+
+(let [c1 (async/chan 10 xform-sum)]
+  (async/go
+    (>! c1 1)
+    (>! c1 2))
+
+  (<!! c1)
+  #_(<!! c1))
