cddf71f9f198c2c7ebaa1f20b8ded58609c89451
Fabio Yamate <fabioyamate@gmail.com>
2020-08-03 14:38:54 -0300

    more samples

--- src/clojure_noob/transducers.clj
+++ src/clojure_noob/transducers.clj
@@ -36,37 +36,58 @@
         (filter my-even?)
         (map (partial my-sum 5))))
 
-(transduce xform-sum
-           vector
-           (range 5))
+(do (println "------------- transduce")
+    (transduce xform-sum
+               vector
+               (range 5)))
 
 ;; traditional way
 
-(->> (range 5)
-     (map my-inc)
-     (filter my-even?)
-     (map (partial my-sum 5))
-     (reduce vector []))
+(do (println "-------------- traditional way")
+    (->> (range 5)
+         (map my-inc)
+         (filter my-even?)
+         (map (partial my-sum 5))
+         (reduce vector [])))
+
+;; if we wanted to mimic the machine
+
+(defn xform-fn
+  [x]
+  (let [x2 (my-inc x)]
+    (when (my-even? x2)
+      (let [x3 (my-sum x2 5)]
+        x3))))
+
+(do (println "-------------- xform-fn")
+    (->> (range 5)
+         (keep xform-fn)
+         (reduce vector [])))
 
 ;; eduction
 
 (def iter (eduction xform-sum (range 5)))
-(reduce vector [] iter)
+
+(do (println "-------------- eduction")
+    (reduce vector [] iter))
 
 ;; into
 
-(into [] xform-sum (range 5))
+(do (println "-------------- into")
+    (into [] xform-sum (range 5)))
 
 ;; sequence
 
-(sequence xform-sum (range 5))
+(do (println "-------------- sequence")
+    (sequence xform-sum (range 5)))
 
 ;; async go channel
 
-(let [c1 (async/chan 10 xform-sum)]
-  (async/go
-    (>! c1 1)
-    (>! c1 2))
+(do (println "-------------- channel")
+    (let [c1 (async/chan 10 xform-sum)]
+      (async/go
+        (>! c1 1)
+        (>! c1 2))
 
-  (<!! c1)
-  #_(<!! c1))
+      (<!! c1)
+      #_(<!! c1)))
