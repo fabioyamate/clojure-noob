71c50358fb1350624ff389ea93c6c6482f6bcfec
Fabio Yamate <fabioyamate@gmail.com>
2021-09-23 00:52:53 -0300

    Evolve dsl

--- src/clojure_noob/promql.clj
+++ src/clojure_noob/promql.clj
@@ -1,5 +1,6 @@
 (ns clojure-noob.promql
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]))
 
 ;; services_http_request_latency_seconds_bucket - metric name time-series
 ;; {} - time-series filter
@@ -40,6 +41,32 @@
       [:single (first value-or-multi-values)])
     [:single value-or-multi-values]))
 
+(defn equal [value]
+  {:match/op "="
+   :match/value value})
+
+(defn not-equal [value]
+  {:match/op "!="
+   :match/value value})
+
+(defn regex-match [value-or-values]
+  {:match/op "=~"
+   :match/value (if (coll? value-or-values)
+                  (->> value-or-values
+                       (remove nil?)
+                       (map name)
+                       (str/join "|"))
+                  value-or-values)})
+
+(defn not-regex-match [value-or-values]
+  {:match/op "!~"
+   :match/value (if (coll? value-or-values)
+                  (->> value-or-values
+                       (remove nil?)
+                       (map name)
+                       (str/join "|"))
+                  value-or-values)})
+
 (defn match
   [matching-labels]
   (str "{"
@@ -56,6 +83,49 @@
   (match {:service "foo" :path ["http"]})
   )
 
+(defn- render-match-op
+  [label {:match/keys [op value]}]
+  (str (name label) op "\"" value "\""))
+
+(defn- labels-filter
+  [filters]
+  (str "{"
+       (str/join ","
+                 (for [[label scalar-or-op] filters
+                       :when (some? scalar-or-op)]
+                   (cond (and (map? scalar-or-op)
+                              (contains? scalar-or-op :match/op))
+                         (render-match-op label scalar-or-op)
+
+                         (coll? scalar-or-op)
+                         (render-match-op label (regex-match scalar-or-op))
+
+                         :else
+                         (render-match-op label (equal scalar-or-op)))))
+       "}"))
+
+(labels-filter {:foo "bar" :bar ["car" :caz nil]
+                :fooz (equal "fooz")
+                :status (equal 200)
+                :active (equal true)
+                :error 300
+                :baaz (not-equal "baaz")
+                :car (regex-match "2.*")
+                :cdr (not-regex-match "5.*")
+                :door (regex-match ["c" "b"])
+                :ignore nil})
+
+(defn metric-selector
+  ([metric-name]
+   metric-name)
+  ([metric-name labels-or-range-value]
+   (if (map? labels-or-range-value)
+     (metric-selector metric-name labels-or-range-value nil)
+     (metric-selector metric-name nil labels-or-range-value)))
+  ([metric-name labels range-value]
+   (str metric-name (labels-filter labels)
+        (when range-value (str "[" range-value "]")))))
+
 (defn escape-parameters
   [parameters]
   (mapv (fn [param]
@@ -70,24 +140,42 @@
 
 (defn render-aggr-op
   [{:aggr-op/keys [op without by parameters vector-expr]}]
-  ;;{:pre [(not (and without by))]}
+
   (apply str
          [(name op)
           "("
           (str/join "," (concat (escape-parameters parameters) [vector-expr]))
           ")"
-          (cond without (str "without" (labels-list without))
-                by (str "by" (labels-list by)))
+          (cond without (str " without " (labels-list without))
+                by (str " by " (labels-list by)))
+
           ]))
+
+(defn sum
+  [vector-selector & {:keys [by without]}]
+  {:pre [(not (and without by))]}
+  (render-aggr-op
+   (cond-> {:aggr-op/op :sum
+            :aggr-op/vector-expr vector-selector}
+     (some? by) (assoc :aggr-op/by by)
+     (some? without) (assoc :aggr-op/without without))))
+
+(defn rate [vector-selector]
+  (render-aggr-op
+   {:aggr-op/op :rate
+    :aggr-op/vector-expr vector-selector}))
+
+
+(sum (rate (metric-selector "http_total" "5m")) :by [:path])
+
 
 
 (comment
-(render-aggr-op {:aggr-op/op :sum
-                 :aggr-op/without [:path "jon"]
-                 :aggr-op/by [:path]
-                                        ;:aggr-op/parameters [5, "foo", true]
-                 :aggr-op/vector-expr "http_total"
-                 })
+  (render-aggr-op {:aggr-op/op :sum
+                   ;;:aggr-op/without [:path "jon"]
+                   :aggr-op/by [:path]
+                   :aggr-op/parameters [5, "foo", true]
+                   :aggr-op/vector-expr "http_total"
+                   })
 
-(render-aggr-op {:aggr-op/op :sum :aggr-op/without ["foo"] :aggr-op/by ["foo"]}))
-
+  (render-aggr-op {:aggr-op/op :sum :aggr-op/without ["foo"] :aggr-op/parameters [5]}))
